d9cd24fc9f62dfba292995c4f594a43e2e5a9b3e
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-11-16 15:01:12 +0000

    Solved Lab

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,6 @@
-function Search() {
+function Search({filter, setFilter}) {
+
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +12,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
         />
       </div>
     </div>
--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -21,12 +23,31 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
-
+  const navigate = useNavigate();
 
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+ function handleSubmit(e) {
+    e.preventDefault();
+    const newBeer = {
+      name,
+      tagline,
+      description,
+      imageUrl,
+      firstBrewed,
+      brewersTips,
+      attenuationLevel,
+      contributedBy
+    }
+
+    axios.post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer)
+    .then(({data}) => console.log(data))
+    .catch((error) => console.error(error))
+    // Navigate to the list of all beers after creating a new beer
+    navigate('/beers')
+  }
 
 
 
@@ -34,7 +55,7 @@ function AddBeerPage() {
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
